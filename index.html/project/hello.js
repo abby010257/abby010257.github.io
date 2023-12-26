da03d6ef4c47a275a7e0a31d6cd15739054679d0
AbbyChang010257 <abby010257@gmail.com>
2023-12-26 21:48:12 +0800

    update

--- index.html/project/hello.js
+++ index.html/project/hello.js
@@ -1,11 +1,47 @@
-var a=8000
-function getnumber(a){
-   if (a>1000000){
-     return ("我嫁給你"); 
-   }else if (a>500000){
-     return ("你嫁給我");
-   }else {
-     return ("存夠錢再來");
-   }
-}
-console.log(getnumber(a))
+//var a=8000
+//function getnumber(a){
+//   if (a>1000000){
+//     return ("我嫁給你"); 
+//   }else if (a>500000){
+//     return ("你嫁給我");
+//   }else {
+//     return ("存夠錢再來");
+//   }
+//}
+//
+//for(var i=0;i<20;i++){
+// if(i%2==0){
+//   console.log(i);  
+// }      
+//} 
+
+//for(var i=0;i<20;i++){
+// if(i%3!=0){
+//   console.log(i);  
+// }      
+//}  
+
+//var i=0;
+//while(i<20){ 
+//   document.write(i+"<br>"); 
+//   i++;   
+//} 
+
+//var i=0;
+//while(i<20){ 
+//   if(i%2!=0){
+//      document.write(i+"<br>");
+//      }
+//    i++;
+//} 
+
+//var i=0;
+//while(i<100){
+//    if(i%7==1){
+//       document.write(i+"<br>");
+//       }
+//    i++;
+//}
+
+
+
